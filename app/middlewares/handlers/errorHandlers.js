2ea132aa362ae435184c84ad5c8e0de29991065b
soukina.nounou@gmail.com <soukina.nounou@gmail.com>
2023-03-29 16:11:15 +0200

    Initialisation de la l'API

---
+++ app/middlewares/handlers/errorHandlers.js
@@ -0,0 +1,18 @@
+exports.catchErrors = (fn) => {
+    return (req, res, next) => {
+        return fn (req, res, next).catch(next);
+        // return fn(req, res, next).catch(err => next(err));
+    }
+}
+
+exports.notFound = (req, res, next) => {
+    const error = new Error(`Page not found`);
+    error.status = 404;
+    next(error);
+}
+
+exports.errorsCollector = (error, req, res, next) => {
+    const status = error.status || 500;
+    error.status = status;
+    return res.status(status).json(`${error}`);
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const router = require('./app/routers');
+
+const { errorsCollector, notFound } = require('./app/middlewares/handlers/errorHandlers');
+
+const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.BASE_URL;
+
+app.use(router);
+
+// error handling
+app.use(notFound);
+app.use(errorsCollector);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${BASE_URL}:${PORT}`)
+});
