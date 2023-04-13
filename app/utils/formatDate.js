684a940bfae88c2276d7bce15c1a4bdfd0d5c626
SebastienMercier91 <sebastien.mercier.91@outlook.com>
2023-04-13 14:49:17 +0200

    New format Date for event

--- app/utils/formatDate.js
+++ app/utils/formatDate.js
@@ -1,9 +1,6 @@
 function formatDate (date) {
     const validFormat = date.split('-').reverse().join('/');
-    console.log(validFormat);
     return validFormat;
 };
-
-formatDate('2023-04-13');
 
 module.exports = formatDate;
